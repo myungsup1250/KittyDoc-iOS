a52c837649ae9e23859e0b120e0f9bf262f0093f
Myungsup Kwak <myungsup1250@gmail.com>
2021-01-15 02:16:58 +0900

    Added Localization, Base Settings... etc

--- KittyDocUIKit/ViewController.swift
+++ KittyDocUIKit/ViewController.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 
+extension String {
+//    func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
+//        return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
+//    }
+    var localized: String {
+        return NSLocalizedString(self, tableName: "Localizable", value: self, comment: "")
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // serveriplable.text = "Bluetooth Alert".localized // "Bluetooth Alert".localized()
     }
 
 
