e2faef28b172988d81fef5cbfe531848d55e7898
Myungsup Kwak <myungsup1250@gmail.com>
2021-01-16 13:51:24 +0900

    .

--- KittyDocUIKit/MainViewController.swift
+++ KittyDocUIKit/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  KittyDocUIKit
 //
 //  Created by 곽명섭 on 2021/01/15.
@@ -16,14 +16,16 @@ extension String {
     }
 }
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.navigationItem.prompt = "UITabBarController"
         // serveriplable.text = "Bluetooth Alert".localized // "Bluetooth Alert".localized()
     }
+    
 
 
 }
