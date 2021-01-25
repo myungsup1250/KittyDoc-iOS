578fe59fd665aa5c0595dd36cf9a6e83fb4164ec
comeheredart <wonee0321@gmail.com>
2021-01-24 21:29:00 -0800

    LoginVC 리팩토링

--- KittyDocUIKit/VC/LogIn/LogInViewController.swift
+++ KittyDocUIKit/VC/LogIn/LogInViewController.swift
@@ -17,14 +17,47 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         
         // Do any additional setup after loading the view.
 
+        let _: UITextField = {
+            emailTF = UITextField(frame: CGRect(x: 0, y: 40, width: 320, height: 40))
+            emailTF.placeholder = "kittydoc@jmsmart.co.kr"
+            emailTF.delegate = self
+            emailTF.autocapitalizationType = .none
+            emailTF.borderStyle = .roundedRect
+            emailTF.clearButtonMode = .whileEditing
+            emailTF.keyboardType = .emailAddress
+            emailTF.enablesReturnKeyAutomatically = true
+            return emailTF
+        }()
+        
+        
+        let _: UITextField = {
+            pwTF = UITextField(frame: CGRect(x: 0, y: 140, width: 320, height: 40))
+            pwTF.placeholder = "password"
+            pwTF.isSecureTextEntry = true
+            pwTF.delegate = self
+            pwTF.borderStyle = .roundedRect
+            pwTF.clearButtonMode = .whileEditing
+            pwTF.enablesReturnKeyAutomatically = true
+            return pwTF
+        }()
+        
+        
         self.navigationItem.prompt = "UITabBarController"
 
-        view.addSubview(welcomeLabel())
-        view.addSubview(guideLabel())
-        view.addSubview(signInView())
-        view.addSubview(signInBtn())
-        view.addSubview(signUpView())
+        view.addSubview(welcomeLabel)
+        view.addSubview(guideLabel)
+        view.addSubview(signInView)
+        view.addSubview(signInBtn)
+        view.addSubview(askLabel)
+        view.addSubview(signUpBtn)
 
+        
+        signInView.addSubview(emailLabel)
+        signInView.addSubview(pswLabel)
+        signInView.addSubview(emailTF)
+        signInView.addSubview(pwTF)
+
+        
         userInfo.Email = "myungsup1250@gmail.com"
         userInfo.Pw = "ms5892"
 
@@ -47,65 +80,56 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         textFieldSetUp()
     }
 
-    func welcomeLabel() -> UILabel {
+    let welcomeLabel: UILabel = {
         let welcomeLabel = UILabel()
 
-        welcomeLabel.frame = CGRect(x: 50, y: 150, width: view.frame.size.width, height: 80)
+        welcomeLabel.frame = CGRect(x: 50, y: 150, width: 400, height: 80)
         welcomeLabel.text = "Welcome".localized
         welcomeLabel.font = welcomeLabel.font.withSize(40)
 
         return welcomeLabel
-    }
+    }()
 
-    func guideLabel() -> UILabel {
+    let guideLabel : UILabel = {
         let guideLabel = UILabel()
 
-        guideLabel.frame = CGRect(x: 50, y: 200, width: view.frame.size.width, height: 40)
+        guideLabel.frame = CGRect(x: 50, y: 200, width: 400, height: 40)
         guideLabel.text = "Sign in to Continue".localized
         guideLabel.textColor = .systemGray
 
         return guideLabel
-    }
-
-    func signInView() -> UIView {
+    }()
+    
+    
+    let signInView : UIView = {
         let signInView = UIView()
-
-        signInView.frame = CGRect(x: 30, y: 300, width: view.frame.size.width, height: 300)
-        let emailLabel = UILabel(frame: CGRect(x: 10, y: 0, width: signInView.frame.size.width, height: 40))
-        emailLabel.text = "Email"
-
-        emailTF = UITextField(frame: CGRect(x: 0, y: 40, width: 320, height: 40))
-        emailTF.placeholder = "kittydoc@jmsmart.co.kr"
-        emailTF.delegate = self
-        emailTF.autocapitalizationType = .none
-        emailTF.borderStyle = .roundedRect
-        emailTF.clearButtonMode = .whileEditing
-        emailTF.keyboardType = .emailAddress
-        emailTF.enablesReturnKeyAutomatically = true
+        signInView.frame = CGRect(x: 30, y: 300, width: 500, height: 300)
         
-        let pswLabel = UILabel(frame: CGRect(x: 10, y: 100, width: signInView.frame.size.width, height: 40))
+        return signInView
+    }()
+    
+    
+    let emailLabel : UILabel = {
+        let emailLabel = UILabel(frame: CGRect(x: 10, y: 0, width: 500, height: 40))
+        emailLabel.text = "Email"
+        
+        return emailLabel
+    }()
+    
+    
+    let pswLabel : UILabel = {
+        let pswLabel = UILabel(frame: CGRect(x: 10, y: 100, width: 500, height: 40))
         pswLabel.text = "Password"
         
-        pwTF = UITextField(frame: CGRect(x: 0, y: 140, width: 320, height: 40))
-        pwTF.placeholder = "password"
-        pwTF.isSecureTextEntry = true
-        pwTF.delegate = self
-        pwTF.borderStyle = .roundedRect
-        pwTF.clearButtonMode = .whileEditing
-        pwTF.enablesReturnKeyAutomatically = true
-        
-        signInView.addSubview(emailLabel)
-        signInView.addSubview(pswLabel)
-        signInView.addSubview(emailTF)
-        signInView.addSubview(pwTF)
+        return pswLabel
+    }()
+    
 
-        return signInView
-    }
-
-    func signInBtn() -> UIButton {
+    
+    let signInBtn : UIButton = {
         let signInBtn = UIButton()
 
-        signInBtn.frame = CGRect(x: view.frame.midX - 150, y: 570, width: 300, height: 50)
+        signInBtn.frame = CGRect(x: 40, y: 570, width: 300, height: 50)
         signInBtn.setTitle("Sign in", for: .normal)
         signInBtn.setTitleColor(.white, for: .highlighted)
         signInBtn.backgroundColor = .systemBlue
@@ -113,29 +137,30 @@ class LogInViewController: UIViewController, UITextFieldDelegate {
         signInBtn.addTarget(self, action: #selector((didTapSignIn)), for: .touchUpInside)
 
         return signInBtn
-    }
+    }()
+    
 
-    func signUpView() -> UIView {
-        let signUpView = UIView()
-
-        signUpView.frame = CGRect(x: 100, y: 640, width: view.frame.size.width, height: 50)
-        let askLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+    let askLabel : UILabel = {
+        let askLabel = UILabel()
+        askLabel.frame = CGRect(x: 100, y: 640, width: 100, height: 50)
         askLabel.text = "New User?"//"Don't have an account?"
+        
+        return askLabel
+    }()
+    
 
+    let signUpBtn : UIButton = {
         let signUpBtn = UIButton()
-        signUpBtn.frame = CGRect(x: 90, y: 0, width: 100, height: 50)
+        signUpBtn.frame = CGRect(x: 190, y: 640, width: 100, height: 50)
         signUpBtn.setTitle("Sign Up", for: .normal)
         signUpBtn.setTitleColor(.systemIndigo, for: .normal)
         signUpBtn.addTarget(self, action: #selector((didTapSignUp)), for: .touchUpInside)
-
-
-        signUpView.addSubview(askLabel)
-        signUpView.addSubview(signUpBtn)
-
-        return signUpView
-    }
+        
+        
+        return signUpBtn
+    }()
+        
     
-
     @objc private func didTapSignUp() {
         let signUp = self.storyboard!.instantiateViewController(identifier: "SignUp")
         signUp.modalPresentationStyle = .fullScreen
