f3c6fa02df6adadd186f081bcd46ab44139abf0a
Myungsup Kwak <myungsup1250@gmail.com>
2021-03-18 19:23:53 +0900

    현우 ServerData 객체 수정 frontTime => startMilliSec (탐색 시작 시점) rearTime => endMilliSec (탐색 종료 시점) startMilliSec가 endMilliSec보다 작아야 함!

--- KittyDocUIKit/Server/Data/AnalysisData.swift
+++ KittyDocUIKit/Server/Data/AnalysisData.swift
@@ -9,19 +9,20 @@ import Foundation
 
 class AnalysisData:ServerData{
     var petID: Int
-    var frontTime: Int
-    var rearTime: Int
+    var startMilliSec: Int  //탐색 시작 시점
+    var endMilliSec: Int    //탐색 종료 시점
+    //startMilliSec가 endMilliSec보다 작아야 함!
     
-    init(_petID: Int, _frontTime: Int, _rearTime: Int){
+    init(_petID: Int, _startMilliSec: Int, _endMilliSec: Int){
         self.petID = _petID
-        self.frontTime = _frontTime
-        self.rearTime = _rearTime
+        self.startMilliSec = _startMilliSec
+        self.endMilliSec = _endMilliSec
     }
     
     func data() -> Data{
         let data:String = "petID" + "=" + String(petID)
-            + "&" + "frontTime" + "=" + String(frontTime)
-            + "&" + "rearTime" + "=" + String(rearTime)
+            + "&" + "startMilliSec" + "=" + String(startMilliSec)
+            + "&" + "endMilliSec" + "=" + String(endMilliSec)
         return data.data(using: String.Encoding.utf8, allowLossyConversion: false)!
     }
 }
