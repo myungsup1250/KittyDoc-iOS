b8790638c45733462ca646bffbb6a4ee2a5974c5
comeheredart <wonee0321@gmail.com>
2021-05-14 12:56:28 +0900

    [ADD] ScaleViewController and SideMenuDelegate

---
+++ KittyDocUIKit/VC/Home/ScaleViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ScaleViewController.swift
+//  KittyDocUIKit
+//
+//  Created by JEN Lee on 2021/05/14.
+//
+
+import UIKit
+
+class ScaleViewController: UIViewController {
+
+    @IBOutlet weak var scaleValueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+
+}
